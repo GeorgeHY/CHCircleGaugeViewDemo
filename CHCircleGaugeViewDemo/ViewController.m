156f4e2b63f4ded113ce96e12c04f297fd20002d
GeorgeHan <hanyangkid@163.com>
2015-05-11 15:06:25 +0800

    first commit

--- CHCircleGaugeViewDemo/ViewController.m
+++ CHCircleGaugeViewDemo/ViewController.m
@@ -7,8 +7,13 @@
 //
 
 #import "ViewController.h"
-
+#import "CHCircleGaugeView.h"
+#import "CircleStatus/NOCSView.h"
+#import "NOCSPercentageColor.h"
+#import "MMPlaceHolder.h"
 @interface ViewController ()
+
+@property (nonatomic, strong) NOCSView * csView;
 
 @end
 
@@ -16,7 +21,16 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    CGRect frame = CGRectMake(0, 0, 200, 200);
+    self.csView = [[NOCSView alloc]initWithFrame:CGRectMake(50, 100, 320, 80)];
+    [self.csView showPlaceHolderWithLineColor:[UIColor blackColor]];
+    [self.view addSubview:self.csView];
+    self.csView.backgroundColor = [UIColor lightGrayColor];
+
+    [self.csView setPercentageColorArray:@[[[NOCSPercentageColor alloc]initWithTitle:@"好评23" color:[UIColor blueColor] percentage:0.5f],[[NOCSPercentageColor alloc]initWithTitle:@"中评15" color:[UIColor greenColor] percentage:0.3f]]];
+    UIView * view = [[UIView alloc]initWithFrame:CGRectMake(80, 0, 60, 80)];
+    view.backgroundColor = [UIColor yellowColor];
+    [self.csView addSubview:view];
 }
 
 - (void)didReceiveMemoryWarning {
