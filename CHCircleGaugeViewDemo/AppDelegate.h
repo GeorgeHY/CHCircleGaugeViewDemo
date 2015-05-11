9210a790a0048fcd601b24ce5529204a78d1f312
GeorgeHan <hanyangkid@163.com>
2015-05-11 14:17:07 +0800

    Initial Commit

---
+++ CHCircleGaugeViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CHCircleGaugeViewDemo
+//
+//  Created by iwind on 15/5/11.
+//  Copyright (c) 2015年 HanYang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CHCircleGaugeViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CHCircleGaugeViewDemo
+//
+//  Created by iwind on 15/5/11.
+//  Copyright (c) 2015年 HanYang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
